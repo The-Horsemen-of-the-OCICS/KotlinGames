0848af52a29fb392761b2017b1701838faa5d347
Wenjiu Wang <vanjor1014@gmail.com>
2022-01-25 15:44:07 -0500

    Deisgn and implement WelcomeActivity, MainActivity and QuizActivity

---
+++ app/src/main/java/com/ocics/kotlingames/viewmodel/ScoreViewModel.kt
@@ -0,0 +1,9 @@
+package com.ocics.kotlingames.viewmodel
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel: ViewModel() {
+
+    var quizGameScore: Long = 0
+    var sheepGameScore: Long = 0
+
+}
