750a6f8ec631a3d4c0c8eaf0c0c5bc26c63d2460
Wenjiu Wang <vanjor1014@gmail.com>
2022-01-24 21:35:26 -0500

    Signed-off-by: Wenjiu Wang <vanjor1014@gmail.com>
    Design and implement Welcome Activity, Main Activity

--- app/src/main/java/com/ocics/kotlingames/MainActivity.kt
+++ app/src/main/java/com/ocics/kotlingames/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.ocics.kotlingames
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.viewModels
+import com.ocics.kotlingames.viewmodel.ScoreViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    private val mScoreViewModel: ScoreViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        mScoreViewModel.quizGameScore = sharedPref.getLong(getString(R.string.quiz_score), 0)
+        mScoreViewModel.sheepGameScore = sharedPref.getLong(getString(R.string.sheep_score), 0)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            Log.d(TAG, "shared pref saves: quiz_score=${mScoreViewModel.quizGameScore}, sheep_score=${mScoreViewModel.sheepGameScore}" )
+            putLong(getString(R.string.quiz_score), mScoreViewModel.quizGameScore)
+            putLong(getString(R.string.sheep_score), mScoreViewModel.sheepGameScore)
+            apply()
+        }
+
+    }
+
+    fun launchQuiz(v: View) {
+        val intent = Intent()
+        intent.setClass(this, QuizActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun launchSheep(v: View) {
+
+    }
+
+    fun launchScore(v: View) {
+
+    }
+
+    fun exit(v: View) {
+        finish()
     }
 }
---
+++ app/src/main/java/com/ocics/kotlingames/WelcomeActivity.kt
@@ -0,0 +1,28 @@
+package com.ocics.kotlingames
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import com.ocics.kotlingames.viewmodel.ScoreViewModel
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+    }
+
+    fun onOkClicked(v: View) {
+        val intent = Intent()
+        intent.setClass(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
